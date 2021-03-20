93763c1973beee84b4aae4cfcfda115886e7877e
linzhenfang <linzhenfang@hztianque.com>
2021-03-21 00:10:27 +0800

    表格添加

--- src/pages/MyQuestion/components/QuestionBraftEditor/index.jsx
+++ src/pages/MyQuestion/components/QuestionBraftEditor/index.jsx
@@ -4,10 +4,25 @@ import 'braft-editor/dist/index.css'
 import { uploadImg } from '@/services/myQuestion/create'
 import styles from './index.less'
 
+import Table from 'braft-extensions/dist/table'
+
+import 'braft-editor/dist/index.css'
+import 'braft-extensions/dist/table.css'
+
+const options = {
+  defaultColumns: 5, // 默认列数
+  defaultRows: 5, // 默认行数
+  withDropdown: true, // 插入表格前是否弹出下拉菜单
+  columnResizable: true, // 是否允许拖动调整列宽，默认false
+  exportAttrString: '', // 指定输出HTML时附加到table标签上的属性字符串
+  // includeEditors: ['id-1'], // 指定该模块对哪些BraftEditor生效，不传此属性则对所有BraftEditor有效
+  // excludeEditors: ['id-2']  // 指定该模块对哪些BraftEditor无效
+};
 
 const QuestionBraftEditor = (props) => {
   const { value, onChange, placeholder, editKey = '' } = props
   console.log('render', value)
+  BraftEditor.use(Table(options));
   const editorChange = (changes) => {
     if (onChange) onChange(changes)
   }
